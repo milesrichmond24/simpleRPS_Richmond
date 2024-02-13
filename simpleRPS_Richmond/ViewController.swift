c6f8686afa489696e82b911d5f80ead79b74347c
milesrichmond <miles.richmond24@d155.org>
2024-02-13 09:34:52 -0600

    Initial Commit

---
+++ simpleRPS_Richmond/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleRPS_Richmond
+//
+//  Created by MILES RICHMOND on 2/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
